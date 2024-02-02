5dafd5705c1f58822956e5952a6febd9ba2d7db5
AdityaPare <adityapare2001@gmail.com>
2024-02-02 17:08:40 +0530

    final styling and coloring

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -4,13 +4,30 @@ const StatusMessage = ({ winner, current }) => {
   const NoMoveLeft = current.board.every(el => el !== null);
 
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}{' '}
-      {!winner &&
-        !NoMoveLeft &&
-        `Next player is ${current.isXNext ? 'X' : 'O'}`}
-      {!winner && NoMoveLeft && 'X and O Tied'}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !NoMoveLeft && (
+        <>
+          `Next player is{' '}
+          <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+            {current.isXNext ? 'X' : 'O'}
+          </span>
+        </>
+      )}
+      {!winner && NoMoveLeft && (
+        <>
+          <span className="text-green">X</span> and{' '}
+          <span className="text-orange">O</span> Tied
+        </>
+      )}
+    </div>
   );
 };
 
